bf9ffd68a10e898560975d14926a895ceefee286
ambar <ambar.lee@gmail.com>
2017-10-17 16:40:14 +0800

    add component spec

---
+++ packages/reiconify/test/component.spec.js
@@ -0,0 +1,63 @@
+const React = require('react')
+const renderer = require('react-test-renderer')
+const path = require('path')
+const {promisify} = require('util')
+const rimraf = require('rimraf')
+const {transform} = require('..')
+
+describe('component', () => {
+  const orignalCwd = process.cwd()
+  const cwd = path.resolve(__dirname, 'fixtures/component')
+  const srcDir = path.resolve(cwd, 'src')
+  let Icons
+
+  beforeAll(async () => {
+    process.chdir(cwd)
+    await transform({
+      inputs: 'icons/*.svg',
+      src: true,
+      srcDir,
+    })
+  })
+
+  beforeEach(() => {
+    Icons = require(srcDir)
+  })
+
+  afterAll(async () => {
+    await promisify(rimraf)(srcDir)
+    process.chdir(orignalCwd)
+  })
+
+  it('renders `size` prop', () => {
+    const tree = renderer.create(<Icons.Check size={20} />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders `className` prop', () => {
+    const tree = renderer.create(<Icons.Check className="myIcon" />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders `text` prop', () => {
+    const tree = renderer.create(<Icons.Check text />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders `center` prop', () => {
+    const tree = renderer.create(<Icons.Check center />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders `text` and `center` prop', () => {
+    const tree = renderer.create(<Icons.Check text center />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders `center` and `style` prop', () => {
+    const tree = renderer
+      .create(<Icons.Check center style={{color: 'red'}} />)
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
