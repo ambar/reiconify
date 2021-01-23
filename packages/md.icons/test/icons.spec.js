da7bd2664ed980803ae9fd430e0b630421fcd8ac
ambar <ambar.lee@gmail.com>
2021-01-23 17:31:55 +0800

    test: fix test in md.icons

--- packages/md.icons/test/icons.spec.js
+++ packages/md.icons/test/icons.spec.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
-import * as Icons from '../src'
+import * as Icons from '../es'
 
 describe('icons', () => {
   it(`renders all icons`, () => {
     for (const [name, Icon] of Object.entries(Icons)) {
-      const tree = renderer.create(<Icon />).toJSON()
+      const tree = renderer.create(React.createElement(Icon)).toJSON()
       expect(tree).toMatchSnapshot(name)
     }
   })
