4e70848cb143d0351b56b22ef0186cd2489544fc
ambar <ambar.lee@gmail.com>
2022-10-26 09:09:00 +0800

    feat: use svgo v3

--- packages/reiconify/lib/svg2jsx.js
+++ packages/reiconify/lib/svg2jsx.js
@@ -1,4 +1,4 @@
-const SVGO = require('svgo')
+const svgo = require('svgo')
 const hash = require('string-hash')
 const JSON5 = require('json5')
 const mapKeys = require('lodash/mapKeys')
@@ -12,23 +12,22 @@ const toCamelCase = (s) =>
 // 也可以用 DOMParser/cheerio 等遍历 DOM，但用 svgo 插件少一点依赖（更快）
 // 并且 React v16+ 不再需要这个处理
 const camelCaseProps = {
-  camelCaseProps: {
-    type: 'perItem',
-    description: 'convert attrs to camel case',
-    params: {},
-    fn(item) {
-      if (item.isElem()) {
-        item.eachAttr((attr) => {
-          if (attr.name.includes('-')) {
-            const newAttr = Object.assign({}, attr, {
-              name: toCamelCase(attr.name),
-            })
-            item.attrs[newAttr.name] = newAttr
-            item.removeAttr(attr.name)
-          }
-        })
-      }
-    },
+  name: 'camelCaseProps',
+  type: 'perItem',
+  description: 'convert attrs to camel case',
+  params: {},
+  fn(item) {
+    if (item.isElem()) {
+      item.eachAttr((attr) => {
+        if (attr.name.includes('-')) {
+          const newAttr = Object.assign({}, attr, {
+            name: toCamelCase(attr.name),
+          })
+          item.attrs[newAttr.name] = newAttr
+          item.removeAttr(attr.name)
+        }
+      })
+    }
   },
 }
 
@@ -58,12 +57,33 @@ const camelCaseNamespacedProps = {
 // https://github.com/svg/svgo/issues/646
 // https://github.com/BohemianCoding/svgo-compressor/blob/develop/src/defaultConfig.js
 const getDefaultSvgoPlugins = ({idPrefix}) => [
-  {removeViewBox: false},
-  {removeDesc: {removeAny: true}},
-  {removeXMLNS: true},
-  {sortAttrs: true},
-  {cleanupIDs: {prefix: idPrefix}},
-  camelCaseNamespacedProps,
+  {
+    name: 'preset-default',
+    params: {
+      overrides: {
+        removeViewBox: false,
+        // // customize default plugin options
+        // inlineStyles: {
+        //   onlyMatchedOnce: false,
+        // },
+        // // or disable plugins
+        // removeDoctype: false,
+      },
+    },
+  },
+  {
+    name: 'removeXMLNS',
+  },
+  {
+    name: 'sortAttrs',
+  },
+
+  // {removeViewBox: false},
+  // {removeDesc: {removeAny: true}},
+  // {removeXMLNS: true},
+  // {sortAttrs: true},
+  // {cleanupIDs: {prefix: idPrefix}},
+  // camelCaseNamespacedProps,
 ]
 
 const defaults = {
@@ -85,17 +105,17 @@ const replaceInlineStyles = (svg) =>
 
 const createOptimizer = (options) => {
   options = Object.assign({}, defaults, options)
-  const plugins = getDefaultSvgoPlugins({idPrefix: options.idPrefix})
-    .concat(options.svgoPlugins)
-    .concat(options.camelCaseProps ? camelCaseProps : [])
-  const svgo = new SVGO({plugins})
+  const plugins = getDefaultSvgoPlugins({idPrefix: options.idPrefix}).concat(
+    options.svgoPlugins
+  )
+  // .concat(options.camelCaseProps ? camelCaseProps : [])
+  // const svgo = new SVGO({plugins})
 
   return (svg) =>
-    svgo
-      .optimize(svg)
-      .then(({data}) =>
-        options.styleToObject ? replaceInlineStyles(data) : data
-      )
+    new Promise((resolve) => {
+      const {data} = svgo.optimize(svg, {plugins})
+      resolve(options.styleToObject ? replaceInlineStyles(data) : data)
+    })
 }
 
 const optimize = (svg, options) => {
